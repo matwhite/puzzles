36eef586ed8b7acb7b6e8d39f2c06f09603a7581
Mat White <github@matbert.com>
2017-12-10 00:53:12 -0700

    Add support for brute force of a solution.

--- aristolib.py
+++ aristolib.py
@@ -1,4 +1,5 @@
 from pprint import pprint as pp
+from random import shuffle
 from collections import defaultdict
 from subprocess import check_output, CalledProcessError
 import re
@@ -22,6 +23,8 @@ REPL:
     set A=B: set a letter in the dictonary;
         format is crypto-letter=decrypted-letter
 
+    unset A: unset a crypted letter
+
     dict: dump out the current dictionary (do this to save your work later)
 
     grep: print out grep commands to be used for searching
@@ -40,6 +43,8 @@ REPL:
     setword CRYPT=FOUND: set a crypto word to a given set of letters;
         useful when "find" finds a few words and you want to try them out
 
+    brute: present a list of brute-forced solutions
+
     to exit: ctrl+d
 '''
 
@@ -48,9 +53,19 @@ class Aristo(object):
 
     stor = {}
     c = ''
+    ph = ''
+    letts = ''
+    lettcount = 0
 
     def __init__(self, c):
         self.c = c
+        self.ph = re.sub(r'[^A-Z\s]', '', c)
+        self.ph = re.sub(r'\s+', ' ', self.ph)
+        self.letts = re.sub(r'\s', '', self.ph)
+        self.lettcount = len(set(self.letts))
+
+    def score(self):
+        return(self.lettcount - len(self.stor.keys()))
 
     def repout(self):
         d = []
@@ -60,12 +75,11 @@ class Aristo(object):
             else:
                 d.append(' ')
 
-        print(self.c)
+        print('%s Score: %s' % (self.c, self.score()))
         print(''.join(d))
 
     def grepout(self):
-        ph = re.sub(r'[^A-Z\s]', '', self.c)
-        for w in ph.split(' '):
+        for w in self.ph.split(' '):
             print("# %s" % w)
             gr = "grep '^"
             for l in w:
@@ -76,14 +90,13 @@ class Aristo(object):
             print("%s$' /usr/share/dict/words" % gr)
 
     def apply_popular(self):
-        ph = re.sub(r'[\-\.,\s]', '', self.c)
         sortlet = {}
         popular = 'ETAOINSHRDLCUMWFGYPBVKJXQZ'
         for i, l in enumerate('ETAOINSHRDLCUMWFGYPBVKJXQZ'):
             sortlet[l] = i
 
         qty = defaultdict(int)
-        for l in ph:
+        for l in self.letts:
             qty[l] += 1
         pp(qty)
 
@@ -91,6 +104,16 @@ class Aristo(object):
         for l in sorted(qty, key=qty.get, reverse=True):
             self.stor[l] = popular[i]
             i += 1
+
+    def is_bad_candidate(self, word):
+        try:
+            check_output(
+                ("grep -i '^%s$' /usr/share/dict/words" % word),
+                shell=True
+            )
+        except CalledProcessError:
+            return 1
+        return 0
 
     def find_candidates(self, word):
         maps = defaultdict(list)
@@ -100,14 +123,17 @@ class Aristo(object):
             if len(maps[l]) < 2:
                 del maps[l]
 
-        gr = "grep '^"
+        gr = "grep -i '^"
         for l in word:
             if l in self.stor:
                 gr += self.stor[l].lower()
             else:
                 gr += '[a-z]'
         try:
-            out = check_output(("%s$' /usr/share/dict/words" % gr), shell=True)
+            out = check_output(
+                ("%s$' /usr/share/dict/words | tr A-Z a-z | uniq" % gr),
+                shell=True
+            )
         except CalledProcessError:
             return []
 
@@ -139,9 +165,66 @@ class Aristo(object):
         return proper
 
     def setword(self, cw, dw):
-        pp([cw, dw])
         for i, l in enumerate(cw):
             self.stor[l.upper()] = dw[i].upper()
+
+    # See if setting this word makes any other crypts fail to find a match
+    def _trylist(self, sary):
+        for ww in sary:
+            swords = self.find_candidates(ww)
+            # If we break matching, bail out and undo word
+            if len(swords) == 0:
+                return(0)
+        return(1)
+
+    # See if setting this word makes any other crypts fail to be words
+    def _trybad(self):
+        for w in self.ph.split(' '):
+            word = ''
+            for l in w:
+                if l in self.stor:
+                    word += self.stor[l].lower()
+                else:
+                    word += '.'
+            res = self.is_bad_candidate(word)
+            if res == 1:
+                return(0)
+        return(1)
+
+    # Order list for bruteforce
+    def pre_bf(self):
+        return sorted(self.ph.split(' '), key=len, reverse=True)
+
+    def bruteforce(self, ary):
+        if len(ary) == 0:
+            return
+
+        erst = [l for l in set(ary[0]) if l not in self.stor.keys()]
+
+        if len(erst) == 0:
+            return self.bruteforce(ary[1:len(ary)])
+
+        # Find list of words for next item in array
+        words = self.find_candidates(ary[0])
+        shuffle(words)
+        for w in words:
+            self.setword(ary[0], w)
+
+            res = self._trylist(ary[1:len(ary)])
+            if res != 1:
+                for l in erst:
+                    self.stor.pop(l)
+                continue
+
+            res = self._trybad()
+            if res != 1:
+                for l in erst:
+                    self.stor.pop(l)
+                continue
+            else:
+                break
+
+        self.bruteforce(ary[1:len(ary)])
 
     def repl(self):
         self.repout()
@@ -164,6 +247,10 @@ class Aristo(object):
             elif re.match(r'^set (.*)', line):
                 let, to = re.match(r'^set (.*)', line).group(1).split('=')
                 self.stor[let] = to
+                self.repout()
+            elif re.match(r'^unset (.*)', line):
+                let = re.match(r'^unset (.*)', line).group(1)
+                self.stor.pop(let)
                 self.repout()
             elif re.match(r'^dict', line):
                 pp(self.stor)
@@ -190,5 +277,15 @@ class Aristo(object):
             elif re.match(r'^help', line):
                 print(helptext)
                 self.repout()
+            elif re.match(r'^brute', line):
+                words = self.pre_bf()
+                self.bruteforce(words)
+                self.repout()
+                for _ in range(1, 5):
+                    self.stor = {}
+                    print(' ')
+                    self.bruteforce(words)
+                    self.repout()
+                print('Done')
             else:
                 print("Buh?")
